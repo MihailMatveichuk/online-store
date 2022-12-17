fdac9c8776d20050ba963dfd2f07d5ab4636007d
Mikhail <mihkailmatveichuk@gmail.com>
2022-12-17 09:39:35 +0300

    feat: add header template and some changes in config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './style.css'
-
-import LOGO from './assets/logo.png'
+import Header from './components/Header'
 import { Clicker } from './Clicker'
 
 export const App = () => {
 
   return (
     <>
-      <h1>
+      {/* <h1>
         React start - {process.env.NODE_ENV} -- {process.env.name}{' '}
-      </h1>
-      <img src={LOGO} alt="logo" width="200" height="200" />
-      <Clicker />
-    </>
+      </h1> */}
+      {/* <img src={LOGO} alt="logo" width="200" height="200" /> */}
+      <Header />
+      {/* <Clicker /> */}
+      </>
   )
 }
