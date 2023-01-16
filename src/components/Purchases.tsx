ead1c67cbf729f2606f7cb5ce84254c256e826f0
Mikhail <mihkailmatveichuk@gmail.com>
2023-01-16 22:22:01 +0300

    add components with path of icons

--- src/components/Purchases.tsx
+++ src/components/Purchases.tsx
@@ -12,6 +12,8 @@ import DropdownSortRating from './DropdownRating';
 import BoxNumberCards from './BoxNumberCards';
 import CheckBox from './brands/CheckBox';
 import { Button } from 'reactstrap';
+import LayoutSmallPicturePath from './icon_path/LayoutSmall';
+import LayoutBigPicturePath from './icon_path/LayoutBig';
 
 let value: IPurchase[];
 
@@ -271,11 +273,9 @@ const Purchases = ({
             fill="currentColor"
             className="grid"
             viewBox="0 0 16 16"
-            onClick={() => {
-              changeLayoutSmall();
-            }}
+            onClick={() => changeLayoutSmall()}
           >
-            <path d="M1 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V2zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2zM1 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V7zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zM1 12a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-2zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-2zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2z" />
+            <LayoutSmallPicturePath />
           </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -288,7 +288,7 @@ const Purchases = ({
               changeLayoutBig();
             }}
           >
-            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+            <LayoutBigPicturePath />
           </svg>
         </GridIcon>
       </SearchAndGridRow>
